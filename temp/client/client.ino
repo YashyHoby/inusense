1895280d4cc2701446ca34587b60ae1daece3929
YashyHoby <yashima.research@gmail.com>
2024-11-09 20:23:20 +0900

    a

--- temp/client/client.ino
+++ temp/client/client.ino
@@ -1,68 +1,68 @@
-#include <WiFiS110.h>
-#include <HTTPClient.h>
+#include <HttpGs2200.h>
+#include <TelitWiFi.h>
+#include <SDHCI.h>  // SpresenseのSDカードライブラリ
 #include "config.h"
-#include "server_config.h"
 
-const char* ssid = AP_SSID;
-const char* password = PASSPHRASE;
-const char* server = HTTP_SRVR_IP; // server_config.hで定義
-const int port = HTTP_PORT;
+#define CONSOLE_BAUDRATE 115200
+#define MP3_FILE_PATH "/mnt/audio/Moning_10s.mp3"  // SDカード上のMP3ファイルのパス
 
-WiFiClient wifiClient;
-HTTPClient http;
+SDClass SD;
+
+TelitWiFi gs2200;
+TWIFI_Params gsparams;
+HttpGs2200 theHttpGs2200(&gs2200);
+HTTPGS2200_HostParams hostParams;
 
 void setup() {
-    Serial.begin(115200);
-    WiFi.begin(ssid, password);
+    pinMode(LED0, OUTPUT);
+    digitalWrite(LED0, LOW);
+    Serial.begin(CONSOLE_BAUDRATE);
 
-    // Wi-Fi接続
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    // SDカードの初期化
+    if (!SD.begin()) {
+        ConsoleLog("SDカードの初期化に失敗しました");
+        while (1);
     }
-    Serial.println("WiFi connected");
 
-    // サーバーとの接続確認
-    Serial.print("Connected to server: ");
-    Serial.println(server);
+    Init_GS2200_SPI_type(iS110B_TypeC);
+    gsparams.mode = ATCMD_MODE_STATION;
+    gsparams.psave = ATCMD_PSAVE_DEFAULT;
+    if (gs2200.begin(gsparams)) {
+        ConsoleLog("GS2200の初期化に失敗しました");
+        while (1);
+    }
+
+    if (gs2200.activate_station(AP_SSID, PASSPHRASE)) {
+        ConsoleLog("WiFiの接続に失敗しました");
+        while (1);
+    }
+
+    hostParams.host = (char *)HTTP_SRVR_IP;
+    hostParams.port = (char *)HTTP_PORT;
+    theHttpGs2200.begin(&hostParams);
+}
+
+void sendMp3File() {
+    File mp3File = SD.open(MP3_FILE_PATH, FILE_READ);  // SDHCIを使ったファイルのオープン
+    if (!mp3File) {
+        ConsoleLog("MP3ファイルのオープンに失敗しました");
+        return;
+    }
+
+    theHttpGs2200.config(HTTP_HEADER_CONTENT_TYPE, "audio/mpeg");
+
+    if (theHttpGs2200.post(HTTP_POST_PATH, NULL)) {
+        while (mp3File.available()) {
+            uint8_t buffer[512];
+            size_t bytesRead = mp3File.read(buffer, sizeof(buffer));
+            theHttpGs2200.read_data(buffer, bytesRead);  // データ送信
+        }
+        theHttpGs2200.end();
+    }
+    mp3File.close();
 }
 
 void loop() {
-    sendMP3Data();
-    delay(5000); // データ送信間隔
-}
-
-void sendMP3Data() {
-    // HTTP接続
-    if (http.begin(wifiClient, String("http://") + server + ":" + port + "/upload")) {
-        http.addHeader("Content-Type", "audio/mpeg");
-
-        // MP3データの読み込み（例: SDカードから）
-        File mp3File = SD.open("/audio/Moning_10s.mp3", FILE_READ);
-        if (!mp3File) {
-            Serial.println("Failed to open MP3 file");
-            return;
-        }
-
-        int fileSize = mp3File.size();
-        uint8_t buffer[512];
-        
-        // HTTP POSTを開始
-        int httpResponseCode = http.sendRequest("POST", NULL, fileSize);
-        if (httpResponseCode > 0) {
-            while (mp3File.available()) {
-                size_t len = mp3File.read(buffer, sizeof(buffer));
-                http.write(buffer, len); // サーバにデータを送信
-            }
-            Serial.println("MP3 data sent successfully");
-        } else {
-            Serial.print("Error on sending POST: ");
-            Serial.println(httpResponseCode);
-        }
-
-        http.end();
-        mp3File.close();
-    } else {
-        Serial.println("Unable to connect to server");
-    }
+    sendMp3File();
+    delay(10000);  // 10秒ごとに送信を試行
 }
--- temp/server/server.py
+++ temp/server/server.py
@@ -1,24 +1,12 @@
 from flask import Flask, request
-from werkzeug.utils import secure_filename
-import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+@app.route('/postData', methods=['POST'])
+def receive_mp3():
+    with open("received_file.mp3", "wb") as f:
+        f.write(request.data)
+    return "File received", 200
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'ファイルが見つかりません', 400
-    file = request.files['file']
-    if file.filename == '':
-        return 'ファイル名が空です', 400
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return 'ファイルのアップロードに成功しました', 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10080)
