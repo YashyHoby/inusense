c39b8f52ebe11c0e40de2674464bf40c610f5740
Marsall-k <haruz.kiku@gmail.com>
2024-12-13 03:42:56 +0900

    なにこれ

---
+++ spresense/arudino/apps/check_max_string/check_max_string.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    // USBシリアル接続を待機
+  }
+  delay(1000);
+}
+
+void loop() {
+  String testString = 
+}
--- spresense/arudino/apps/uart_test/uart_test.ino
+++ spresense/arudino/apps/uart_test/uart_test.ino
@@ -1,53 +1,84 @@
-void setup() {
-  // USBシリアル（デバッグ用）
-  Serial.begin(9600);
-  while (!Serial) {
-    // USBシリアル接続を待機
-  }
-  Serial.println("Spresense UART Communication Example");
 
-  // ハードウェアUART (Serial2) の初期化
-  Serial2.begin(9600); // ボーレートを9600bpsに設定
+void setup() {
+  pinMode(2, INPUT);  // LSIが発音中か確認するピン（発音中はLOWになる）
+
+  
+  Serial.begin(9600);   // USBシリアル
+  Serial2.begin(9600);  // ハードウェアUART (Serial2)
 
   delay(1000);
-
-  Serial.println("plz send 1 to start talking");
-  while(1){
-    if (Serial.available()) {
-      String receivedData = Serial.readString();
-      receivedData.trim(); // 空白や改行を削除
-      if (receivedData == "1") {
-        break;
-      }
-    }
-  }
-  
 }
 
 void loop() {
-  Serial2.print("ohayo-.\r");
-  Serial.println("send OHAYOU to Aquestalk LSI");
-
-  /* 再生が終わるまで待機 */
+  /* PCからUSBシリアル経由で文字列を取得 */
+  String response;
+  Serial.println("Send something");
   while(1){
-    if (Serial2.available()) {
-      String receivedData = Serial2.readString(); // 受信データを文字列として読み取る
-      //receivedData.trim(); // 空白や改行を削除
-      if (receivedData == ">") {
-        Serial.println("sound end & Aquestalk LSI ready");
-        break;
-      } else if (receivedData == "*") {
-        Serial.println("ATP3011F4 busy");
-      } else {
-        Serial.print("unknown data received via UART1: ");
-        Serial.println(receivedData);
-      }
+    if (Serial.available()) {
+      response = Serial.readString();
+      response.trim(); // 空白や改行を削除
+      Serial.println(response); // 同じ文字を返答
+      break;
     }
-    delay(1000);
-    Serial.println("now talking...");
-    Serial2.print("\r");  // 状態を確認[CR]
   }
 
-  delay(1000); // 1秒待機
+  /* 文字列の長さを確認，「,」「.」まで，もしくは127バイトまで取り出しながら送信 */
+  int maxLength = 125;
+  int strLength = response.length();  // responseの長さを取得
+  while(strLength > 0) {
+    String subResponse;
 
+    /* 最初に登場する「,」あるいは「.」を探してそこまで送信する */
+    int commaIndex = response.indexOf(","); 
+    int dotIndex = response.indexOf(".");
+    if (commaIndex != -1 && commaIndex < maxLength) {
+      subResponse = response.substring(0, commaIndex+1);
+      response = response.substring(commaIndex+1);
+    } else if (dotIndex != -1 && dotIndex < maxLength) {
+      subResponse = response.substring(0, dotIndex+1);
+      response = response.substring(dotIndex+1);
+    }
+
+    /* 「,」も「.」も見つからなかった場合，あるいはあってもそこまでに127文字を超えてしまう場合 */
+    /* maxLength以前で最も最後に出てくる母音を探してそこまで送信 */
+    else if (strLength > maxLength) {
+      int vowelsIndex = maxLength;
+      /* 127文字目手前の母音を探す */
+      const char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+      for (int i = maxLength - 1; i >= 0; i--) {            // 終了位置から逆方向に検索
+        char currentChar = toLowerCase(response.charAt(i)); // 一応小文字に変換して比較
+        for (int j = 0; j < 5; j++) {                       // 母音リストをチェック
+          if (currentChar == vowels[j]) {
+            vowelsIndex = i;                                // 最初に見つけた位置（最後に現れる母音の位置）をvowelsIndexに代入して終了
+            break;
+          }
+        }
+        if (vowelsIndex == i) break;  // ネストを抜けるやり方がわかりませんでした．
+      }
+
+      subResponse = response.substring(0, vowelsIndex+1); // 送信する分の文字列を取り出してsubResponseに格納
+      response = response.substring(vowelsIndex+1);       // 取り出した文字列を削除して詰める
+    } else {
+      subResponse = response.substring(0, strLength);   // 最後の文字列
+      response = response.substring(strLength);
+    }
+
+    Serial2.print(subResponse); // 最初の文字列を送信
+    Serial2.print("\r");        // 再生
+
+    /* 送信が終わるまで待機 */
+    delay(200);
+    while(1){
+      if (digitalRead(2) == 1){
+        break;
+      } else {
+        Serial.println("busy");
+        delay(100);
+      } 
+    }
+
+    strLength = response.length();    // responseの長さを再度取得
+  }
+
+  // delay(100);
 }
